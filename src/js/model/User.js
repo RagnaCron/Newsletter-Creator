1c6bb19cd06b8817b99443b810388f8e9e198e84
Manuel <Werder>
2019-12-18 15:11:15 +0100

    changes to Model classes

--- src/js/model/User.js
+++ src/js/model/User.js
@@ -1,10 +1,12 @@
 "use strict";
 
 class User {
-	constructor(name, email, password) {
+	constructor(name, email, birthday, password) {
 		this.name = name;
 		this.email = email;
+		this.birthday = birthday;
 		this.password = password;
 	}
 }
+
 module.exports = User;
