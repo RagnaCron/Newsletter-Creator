f1d193b7c6a84f2ef0d7e32bd3642b1c6591a098
Manuel <Werder>
2019-12-24 18:25:18 +0100

    validation on name, email, birthday

--- src/js/windows/Register.js
+++ src/js/windows/Register.js
@@ -30,14 +30,16 @@ const nameError = document.getElementById("name-error");
 regName.onblur = () => {
 	const error = "Enter a Username with more then 4 and less then 12 characters.";
 	inputValidationCheck(regName, nameError, error);
+	// connectToDB(containsUsername);
 };
 regName.onfocus = () => message(nameError, "");
 
 const regEmail = document.getElementById("reg-email");
-const emailError  = document.getElementById("email-error");
+const emailError = document.getElementById("email-error");
 regEmail.onblur = () => {
 	const error = "Enter a correct email address.";
 	inputValidationCheck(regEmail, emailError, error);
+	// connectToDB(containsEmail);
 };
 emailError.onfocus = () => message(emailError, "");
 
@@ -50,26 +52,49 @@ regBirthday.onblur = () => {
 regBirthday.onfocus = () => message(birthdayError, "");
 
 const regPassword1 = document.getElementById("reg-password1");
+const password1Error = document.getElementById("password1-error");
+regPassword1.onblur = () => {
+	const error = "Enter a password, it needs at least 8 sings, max 24.";
+	inputValidationCheck(regPassword1, password1Error, error);
+};
+regPassword1.onfocus = () => message(password1Error, "");
+
 const regPassword2 = document.getElementById("reg-password2");
+const password2Error = document.getElementById("password2-error");
+regPassword2.onblur = () => {
+	const error = "Confirm you last password.";
+	inputValidationCheck(regPassword2, password2Error, error);
+	checkPasswords();
+};
+regPassword2.onfocus = () => message(password2Error, "");
+
+function checkPasswords() {
+	if (regPassword1.value === regPassword2.value)
+		return true;
+	else {
+		message(password1Error, "Password mismatch.");
+		message(password2Error, "Password mismatch.");
+		return false;
+	}
+
+}
 
 let valid = false;
 const registerButton = document.getElementById("reg-button");
 registerButton.addEventListener("click", () => {
-	if (valid) {
-		connectToDB(addUser);
+	connectToDB(containsUsername);
+	connectToDB(containsEmail);
+	if (checkPasswords()) {
+		if (valid) {
+			connectToDB(addUser);
+		}
 	}
 });
+
 
 function connectToDB(dbCallback) {
 	console.log("opening DB ...");
 	const request = indexedDB.open(DB_NAME, DB_VERSION);
-	request.onsuccess = () => {
-		console.log("opening DB DONE");
-		dbCallback(request.result);
-	};
-	request.onerror = () => {
-		console.error("opening DB ERROR:", request.error);
-	};
 	request.onupgradeneeded = () => {
 		console.log("opening DB onupgradeneeded");
 		if (!request.result.objectStoreNames.contains(DB_STORE_USERS)) {
@@ -78,6 +103,13 @@ function connectToDB(dbCallback) {
 			userObjectStore.createIndex('userName', 'userName', {unique: true});
 			userObjectStore.createIndex('email', 'email', {unique: true});
 		}
+	};
+	request.onsuccess = () => {
+		console.log("opening DB DONE");
+		dbCallback(request.result);
+	};
+	request.onerror = () => {
+		console.error("opening DB ERROR:", request.error);
 	};
 }
 
@@ -88,32 +120,42 @@ function addUser(db) {
 	const request = userObjectStore.add(user);
 	request.onerror = () => {
 		console.error("Adding User ERROR:", request.error);
+		message(nameError, "Username is taken.");
+		message(emailError, "Email address is taken.");
 	};
 	request.onsuccess = () => {
 		console.log("Adding User success:", request.result);
 	};
 }
 
-// function containsUsername(db) {
-// 	let is = false;
-// 	const transaction = db.transaction([DB_STORE_USERS], "readonly");
-// 	const userObjectStore = transaction.objectStore(DB_STORE_USERS);
-// 	const index = userObjectStore.index(regName.value);
-// 	index.get(regName.value).onsuccess = (evt) => {
-// 		is = true;
-// 	};
-// 	return is;
-// }
-//
-// function containsEmail(db) {
-// 	let is = false;
-// 	const transaction = db.transaction([DB_STORE_USERS], "readonly");
-// 	const userObjectStore = transaction.objectStore(DB_STORE_USERS);
-// 	const index = userObjectStore.index(regEmail.value);
-// 	index.get(regEmail.value).onsuccess = (evt) => {
-// 		is = true;
-// 	};
-// 	return is;
-// }
+function containsUsername(db) {
+	console.log("Contains user name...");
+	const transaction = db.transaction([DB_STORE_USERS], "readonly");
+	const userObjectStore = transaction.objectStore(DB_STORE_USERS);
+	const index = userObjectStore.index('userName');
+	const request = index.get(regName.value);
+	request.onsuccess = () => {
+		console.log("contains username");
+		message(nameError, "Username is taken.");
+	};
+	request.onerror = () => {
+		console.log("username is free");
+		valid = true;
+	};
+}
 
-
+function containsEmail(db) {
+	console.log("Contains email address");
+	const transaction = db.transaction([DB_STORE_USERS], "readonly");
+	const userObjectStore = transaction.objectStore(DB_STORE_USERS);
+	const index = userObjectStore.index('email');
+	const request = index.get(regEmail.value);
+	request.onsuccess = () => {
+		console.log("contains email address");
+		message(emailError, "Email address is taken.");
+	};
+	request.onerror = () => {
+		console.log("email address is free");
+		valid = true;
+	};
+}
