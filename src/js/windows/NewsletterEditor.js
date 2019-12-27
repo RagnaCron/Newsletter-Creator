082eeb9b08c529c1df1de83a457e225aac4f5e91
Manuel <Werder>
2019-12-27 18:02:06 +0100

    deleting objects form the DOM over drag and drop

--- src/js/windows/NewsletterEditor.js
+++ src/js/windows/NewsletterEditor.js
@@ -10,35 +10,52 @@ const twoColumns = document.getElementById("two-columns");
 const panelThree = document.getElementById("three-columns");
 const line = document.getElementById("line");
 const placeholder = document.getElementsByClassName("placeholder")[0];
+const emptyFolder = document.getElementById("empty-folder");
 
 let draggingElement = null;
 let counter = 0;
 
-oneColumn.addEventListener('dragstart', () => {
+oneColumn.addEventListener("dragstart", () => {
 	draggingElement = 1;
 });
 
-twoColumns.addEventListener('dragstart', () => {
+twoColumns.addEventListener("dragstart", () => {
 	draggingElement = 2;
 });
 
-panelThree.addEventListener('dragstart', () => {
+panelThree.addEventListener("dragstart", () => {
 	draggingElement = 3;
 });
 
-line.addEventListener('dragstart', () => {
+line.addEventListener("dragstart", () => {
 	draggingElement = 4;
 });
 
 let editors = [];
+let columnsDraggingEvents = [];
+let elementForBin;
 
-placeholder.addEventListener('drop', () => {
+function setElementForBin(element) {
+	elementForBin = element;
+}
+
+function columnsEvents(id) {
+	const element = document.getElementById(id);
+	element.addEventListener("dragstart", () => {
+		setElementForBin(element);
+		console.log("added event dragstart");
+	});
+}
+
+placeholder.addEventListener("dragover", (evt) => evt.preventDefault());
+placeholder.addEventListener("drop", () => {
 	if (draggingElement === 1) {
 		document.getElementsByClassName("insertion-area")[0].innerHTML +=
-			`<div id="${counter}" class="mjml-columns ${draggingElement}"> 
+			`<div id="${counter}" class="mjml-columns ${draggingElement}" draggable="true"> 
 				<div style="width: 98%" class="froala-editor" id="editor${counter + 1}"></div>
 			</div>`;
 		editors.push(new FroalaEditor(`#editor${counter + 1}`));
+		columnsEvents(counter);
 		counter = counter + 2;
 	} else if (draggingElement === 2) {
 		document.getElementsByClassName("insertion-area")[0].innerHTML +=
@@ -68,9 +85,13 @@ placeholder.addEventListener('drop', () => {
 	// parse();
 });
 
-placeholder.addEventListener('dragover', (evt) => {
-	evt.preventDefault();
+emptyFolder.addEventListener("dragover", (evt) => evt.preventDefault());
+emptyFolder.addEventListener("drop", (evt) => {
+	elementForBin.removeEventListener("dragstart", setElementForBin);
+	document.getElementsByClassName("insertion-area")[0].removeChild(elementForBin);
+	elementForBin = null;
 });
+
 
 function parse() {
 	const mjmlElements = document.getElementsByClassName("mjml-columns");
